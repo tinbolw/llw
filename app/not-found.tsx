b0df81f59f10278c5ca5056ef37e7cb301031a44
Thomas Van <tinbolw@gmail.com>
2024-09-07 20:02:28 -0700

    favicon, custom 404, metadata

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Custom404() {
+  return (
+    <main>
+      {/* <p className="md:text-3xl text-center">404 - Page Not Found</p> */}
+      <Link href="/">
+        <span className="md:text-xl">Return</span>
+      </Link>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    // does template include html/body?
     <main>
-      <h1>
-        Ling Ling Website
-      </h1>
+      <div>
+        <p className="md:text-3xl text-center">Ling Ling Website</p>
+        <p className="text-center">
+          Welcome to the Ling Ling Website. Made possible with:
+        </p>
+        <ul className="text-center">
+          <li>Next.js</li>
+          <li>MongoDB</li>
+          <li>etc</li>
+        </ul>
+        <p className="md:text-2xl text-center">Check out this stuff:</p>
+      </div>
+      {/* use docs for class */}
+      <div className="flex flex-row justify-center items-center">
+        <Link href="/tools">
+          <span className="text-xl">Tools</span>
+        </Link>
+      </div>
     </main>
-  )
+  );
 }
