8720a00866f667484707471b853f8f65ddcb719d
tinbolw <tinbolw@gmail.com>
2024-09-12 22:09:43 -0700

    fixed sentientLink types

--- app/ui/sentientlink.tsx
+++ app/ui/sentientlink.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import { usePathname } from "next/navigation"
 
 // is there a better way of inheriting className?
-export function SentientLink({href, label, className=''}) {
+export function SentientLink({href, label, className=''}:{href:string, label:string, className:string}) {
   const pathName = usePathname();
   return (
     <Link href={href} className={className}>
